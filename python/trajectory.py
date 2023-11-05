d1d971284f79b4c54afd18305c773c040aa40453
Zack Dufresne <ZADUF@ulaval.ca>
2023-11-04 21:26:10 -0400

    fix array

--- python/trajectory.py
+++ python/trajectory.py
@@ -1,7 +1,7 @@
 import numpy as np
 from game_message import *
 
-def ProjectileToMatrix(projectile: Projectile)->matrix:
-    position = np.matrix(f'{projectile.position.x} {projectile.position.x}')
-    velocity = np.matrix(f'{projectile.velocity.x} {projectile.velocity.x}')
-    return (position,velocity)
+def ProjectileToMatrix(projectile: Projectile)->np.array:
+    position = np.array([projectile.position.x, projectile.position.x])
+    velocity = np.array([projectile.velocity.x, projectile.velocity.x])
+    return (position, velocity)
