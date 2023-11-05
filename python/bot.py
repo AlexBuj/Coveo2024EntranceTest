8a08805a528277f2b60b87b736cc1baf45847082
Parazyte <nilambert6@gmail.com>
2023-11-04 21:35:17 -0400

    intersection calc + basic search

--- python/bot.py
+++ python/bot.py
@@ -1,6 +1,6 @@
 from game_message import *
 from actions import *
-
+import numpy as np
 
 class Bot:
     def __init__(self):
@@ -11,12 +11,16 @@ class Bot:
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
-        if game_message.cannon.orientation >= 45:
-            self.direction = -1
-        elif game_message.cannon.orientation <= -45:
-            self.direction = 1
+        initVelocity = (1,1)
+        initPosition = (0,0)
+        for theta in range(-90,90):
+            velProj = initVelocity @ np.array([[np.cos(0.5*theta/np.pi()),0],[0,[np.sin(0.5*theta/np.pi()),0]]])
+            posSolve = posMeteor - initPosition
+            matrixSolve = np.array([velProj, velMeteor])
+            intersect = np.linalg.solve(matrixSolve,posSolve.T)
 
-        return [
-            RotateAction(angle=15 * self.direction),
-            ShootAction(),
-        ]
+            if intersect[0] >1000 or intersect[1] > 1000 or intersect[0] <0 or intersect[1] <0:
+                continue
+            if matrixSolve[0][0] * intersect[0] + initPosition[0] >0 or matrixSolve[1][1] * intersect[1] + initPosition[1] >0:
+                continue
+            return theta
