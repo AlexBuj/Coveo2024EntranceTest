b6a8e5785c348f279bc4a783b6ebf7b906c8a3c2
AlexBuj <ALBUJ7@ulaval.ca>
2023-11-04 21:21:33 -0400

    add bullshit

--- python/MeteorTargeted.py
+++ python/MeteorTargeted.py
@@ -12,6 +12,16 @@ class MeteorTarget:
             if (meteor.id == id):
                 self.meteors.remove(meteor)
                 self.meteors.append(meteor)
+
+    def addNewMeteorToList(self, newMeteorList):
+        idOccurence = 0;
+        for newMeteor in newMeteorList:
+            for meteorID in self.targetedID:
+                if(newMeteor.id == meteorID):
+                    idOccurence
+            if(idOccurence == 0):
+                self.targetedID.append(newMeteor)
+            idOccurence = 0
     def addMeteor(self, meteor):
         self.meteors.append(meteor)
     def updateMeteorList(self, meteorList):
